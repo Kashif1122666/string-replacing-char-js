2f476c7f9fda59884c4e6c3b9666f35c52e54ab0
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2024-07-26 14:31:56 -0700

    Add files via upload
    string-replacing-char-js

---
+++ string-replacing-char-js/index.js
@@ -0,0 +1,39 @@
+// CHAPTER 25: Strings: Replacing character 
+
+// method : replace
+
+// example
+
+// const fruit = "apple";
+// const replaced = fruit.replace("a","A");
+// console.log(fruit);
+// console.log(replaced);
+
+//  const exampletext = "example text ";
+//  const modified = exampletext.replace("example","practice");
+//  console.log(exampletext);
+//  console.log(modified);
+
+// const text = "i am good";
+// const afterReplace = text.replace("i", "");
+//  console.log(text);
+//  console.log(afterReplace);
+
+// const greetings = "how are you doing?";
+// const afterReplace = greetings.replace("doing?","doing buddy?");
+// console.log(greetings);
+// console.log(afterReplace);
+
+// replace unwanted text(but it will happen one time)
+// let text = "excuse me shut up i am my own boss shut up" ;
+// let cleantext = text.replace("shut up","...");
+// console.log(text);
+// console.log(cleantext);
+
+// replace unwanted text(but it will happen globally)
+// let text = "excuse me shut up i am my own boss shut up" ;
+// let cleantext = text.replace(/shut up/g,"...");
+// console.log(text);
+// console.log(cleantext);
+
+
